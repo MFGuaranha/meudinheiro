45279aea1506abd0c4eb8f27378236af1bdcc2d7
Manoel Franciso Guaranha <manoel.guaranha@gmail.com>
2026-07-25 17:42:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,8 @@ with tab1:
     @st.cache_data(ttl=600, show_spinner="Buscando registros financeiros estáveis...")
     def buscar_gastos_hibrido(id_deputado, nome_deputado, ano):
         # Tentativa 1: Endpoint Direto da API do Governo
-        url_api = f"https://camara.leg.br{id_deputado}/despesas?ano={ano}&itens=100"
+        #url_api = f"https://camara.leg.br{id_deputado}/despesas?ano={ano}&itens=100"
+        url_api = f"https://camara.leg.br{id_deputado}/despesas?ano={ano}"
         headers = {"Accept": "application/json", "User-Agent": "Mozilla/5.0"}
         
         try:
